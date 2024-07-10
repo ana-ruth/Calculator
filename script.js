05e24680987dbdafaa4be1a67a719c73bfef1cd1
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-07-09 17:12:56 -0700

    operate function to call math functions

--- script.js
+++ script.js
@@ -6,8 +6,30 @@ function multiply(num1, num2){ return num1 * num2;}
 
 function divide(num1, num2){ return num1 / num2;}
 
+let firstNum, secondNum, operator;
 
+function operate(num1, operator, num2){
+    let result;
+    switch(operator)
+    {
+        case "addition":
+            result = add(num1,num2);
+            break;
 
+        case "subtraction":
+            result = subtract(num1,num2);
+            break;
+
+        case "multiplication":
+            result = multiply(num1,num2);
+            break;
+
+        case "division":
+            result = divide(num1,num2);
+            break;
+    }
+    return result;
+}
 
 
 
